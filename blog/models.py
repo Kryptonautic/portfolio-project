2927f89edbd49bbdff98ee29827c8af58dc4810e
Wes Dellinger <wes@wes.com>
2020-01-27 22:41:13 -0900

    Blog functional and polished with fake info

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 
-# Create A Blog model
-# title
-# pub_date
-# body
-# image
 class Blog(models.Model):
 	title = models.CharField(max_length=255)
 	pub_date = models.DateTimeField()
 	body = models.TextField()
 	image = models.ImageField(upload_to='images/')
+
+	def __str__(self):
+		# name used in Django admin to display
+		return self.title
+
+	def summary(self):
+		# return first 100 characters
+		return self.body[:100]
+
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%b %e %Y')
 
 # Add the Blog app to the settings
 
