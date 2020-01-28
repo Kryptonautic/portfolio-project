54181d57da7775b668c39dde35a9c14489abe179
Wes Dellinger <wes@wes.com>
2020-01-27 23:35:26 -0900

    Blog updated and cleaned for deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'W3passwd',
+        'PASSWORD': 'django1234',
         'HOST': 'localhost',
         'PORT': '5432',
     }
